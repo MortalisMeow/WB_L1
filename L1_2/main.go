8b94236eb1846e1ab6030d121ccc1dbc3fc75efb
MortalisMeow <msh4253@gmail.com>
2025-08-28 20:34:22 +0300

    done l2.2

--- L1_2/main.go
+++ L1_2/main.go
@@ -1,1 +1,25 @@
-package L1_2
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func main() {
+	var wg sync.WaitGroup
+	list := [5]int{2, 4, 6, 8, 10}
+
+	for index, elem := range list {
+		wg.Add(1)
+		go func(index int, elem int) {
+			defer wg.Done()
+			list[index] = elem * elem
+		}(index, elem)
+	}
+
+	wg.Wait()
+
+	for _, elem := range list {
+		fmt.Println(elem)
+	}
+}
