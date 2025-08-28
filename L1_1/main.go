3d7fac893330f959fdc096b03e77925b99e88056
MortalisMeow <msh4253@gmail.com>
2025-08-28 17:09:10 +0300

    L1.1

---
+++ L1_1/main.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+type Human struct {
+	name string
+	age  int
+}
+
+func (h *Human) Add(name string, age int) *Human {
+	h.name = name
+	h.age = age
+	return h
+
+}
+
+type Action struct {
+	Human
+}
+
+func main() {
+
+	act := Action{
+		Human: Human{
+			name: "Chel",
+			age:  18,
+		},
+	}
+	act.Add("Chelovek", 20)
+
+	fmt.Println(act.name, act.age)
+}
