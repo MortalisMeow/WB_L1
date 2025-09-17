b303f8d81d09dae4f7135de32ae5b55c576ba145
MortalisMeow <msh4253@gmail.com>
2025-09-17 21:58:56 +0300

    done l6

--- L1_5/main.go
+++ L1_5/main.go
@@ -1,1 +1,42 @@
-package L1_5
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+
+	time := time.NewTimer(3 * time.Second)
+	defer time.Stop()
+	ch := make(chan int)
+	val := 1
+
+	go func() {
+		for {
+			select {
+			case <-time.C:
+				return
+			case res, ok := <-ch:
+				if !ok {
+					time.Stop()
+					return
+				}
+				fmt.Printf("result : %d\n", res)
+
+			}
+		}
+
+	}()
+
+	for {
+		select {
+		case <-time.C:
+			return
+		case ch <- val:
+			val++
+		}
+
+	}
+
+}
