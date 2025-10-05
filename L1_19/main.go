40edceb7c6d737dcd7579d27ca3a7bbeb0d7e139
MortalisMeow <msh4253@gmail.com>
2025-10-05 19:35:22 +0300

    done l19

--- L1_19/main.go
+++ L1_19/main.go
@@ -1,1 +1,18 @@
-package L1_19
+package main
+
+import "fmt"
+
+func main() {
+
+	var input string
+	fmt.Printf("Введите строку: ")
+	fmt.Scan(&input)
+
+	str := []rune(input)
+	newstr := make([]rune, len(str))
+
+	for i := 0; i < len(str); i++ {
+		newstr[(len(str)-1)-i] = str[i]
+	}
+	fmt.Println(string(newstr))
+}
